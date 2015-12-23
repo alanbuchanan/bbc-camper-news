2bee2fb56233543c59255f7b5519e107bc4472bd
Rory <rory88@gmail.com>
2015-12-23 20:20:03 +0000

    refactored with es6, populated further with news stories

--- app.js
+++ app.js
@@ -1,167 +1,140 @@
-var Header = React.createClass({
-    render() {
-        return (
-            <div>
-                <div className="white-header">
-                    <span>F</span>
-                    <span>C</span>
-                    <span>C</span>
-                </div>
-                <div className="red-header">
-                    <h1>NEWS</h1>
-                </div>
+var Header = (props) => {
+    return (
+        <div>
+            <div className="white-header">
+                <span>F</span>
+                <span>C</span>
+                <span>C</span>
             </div>
-        );
-    }
-});
-
-
-var DatedListNoPics = React.createClass({
-    render () {
-        return (
-            <ul className="dated-list">
-                <li className="col-sm-6">
-                    <h3>Headline</h3>
-
-                    <p>time | link</p>
-                </li>
-                <li className="col-sm-6">
-                    <h3>Headline</h3>
-
-                    <p>time | link</p>
-                </li>
-                <li className="col-sm-6">
-                    <h3>Headline</h3>
-
-                    <p>time | link</p>
-                </li>
-                <li className="col-sm-6">
-                    <h3>Headline</h3>
-
-                    <p>time | link</p>
-                </li>
-                <li className="col-sm-6">
-                    <h3>Headline</h3>
-
-                    <p>time | link</p>
-                </li>
-                <li className="col-sm-6">
-                    <h3>Headline</h3>
-
-                    <p>time | link</p>
-                </li>
-            </ul>
-        );
-    }
-});
-
-var DatedListWithPics = React.createClass({
-    render () {
-        return (
-            <div className="col-md-6 col-lg-12">
-                <div className="col-md-6">
-                    <img src="http://placehold.it/100x55" alt=""/>
-                </div>
-                <div className="col-md-6">
-                    <h3>Headline</h3>
-
-                    <p>time | link</p>
-                </div>
-
-                <div className="col-md-6">
-                    <img src="http://placehold.it/100x55" alt=""/>
-                </div>
-                <div className="col-md-6">
-                    <h3>Headline</h3>
-
-                    <p>time | link</p>
-                </div>
+            <div className="red-header">
+                <h1>NEWS</h1>
             </div>
-        );
-    }
-});
+        </div>
+    );
+};
 
-var Loading = React.createClass({
-    render () {
-        return (
-            <div>
-                <h1>LOADING</h1>
-            </div>
-        )
-    }
-});
+var Loading = (props) => {
+    return (
+        <div>
+            <h1></h1>
+        </div>
+    )
+};
 
+var TimeAndLink = (props) => {
 
-var SmallStory = React.createClass({
-    render () {
-        return (
-            <div className="small-story">
-                <h3>{this.props.newsItems.headline}</h3>
-                <TimeAndLink time={this.props.newsItems.timePosted} author={this.props.newsItems.author.username}/>
-            </div>
-        );
-    }
-});
+    const link = "http://www.freecodecamp.com/" + props.author;
+    const timeago = $.timeago(props.time).replace(/(about)/gi, '');
 
+    return (
+        <p className="TimeAndLink">
+            <span id="timeago">{timeago}</span> | <a href={link}>{props.author}</a>
+        </p>
+    )
+};
 
-var MediumStory = React.createClass({
-    render () {
-        return (
-            <div className="medium-story col-sm-4 col-xs-12">
-                <img src={this.props.newsItems.image} alt=""/>
+var HeadlineLink = (props) => {
+    return (
+        <div className="HeadlineLink">
+            <a href={props.link}>{props.headline}</a>
+        </div>
+    )
+};
 
-                <h3>{this.props.newsItems.headline}</h3>
+var SmallStory = (props) => {
+    return (
+        <div className="small-story">
+            <h4>{props.newsItems.headline}</h4>
+            <TimeAndLink time={props.newsItems.timePosted} author={props.newsItems.author.username}/>
+        </div>
+    );
+};
 
-                <p>{this.props.newsItems.metaDescription}</p>
-                <TimeAndLink time={this.props.newsItems.timePosted} author={this.props.newsItems.author.username}/>
-            </div>
-        );
-    }
-});
+var MediumStory = (props) => {
+    return (
+        <div className="medium-story col-sm-4 col-xs-12">
+            <img src={props.newsItems.image} alt=""/>
 
-var TimeAndLink = React.createClass({
-    render () {
+            <h4>{props.newsItems.headline}</h4>
 
-        var link = "http://www.freecodecamp.com/" + this.props.author;
-        var timeago = $.timeago(this.props.time).replace(/(about)/gi, '');
+            <p>{props.newsItems.metaDescription}</p>
+            <TimeAndLink time={props.newsItems.timePosted} author={props.newsItems.author.username}/>
+        </div>
+    );
+};
 
-        return (
-            <p>
-                {timeago} | <a href={link}>{this.props.author}</a>
-            </p>
-        )
-    }
-})
+var BigStory = (props) => {
 
-var BigStory = React.createClass({
-
-    render () {
-
-        let {headline} = this.props.newsItems;
+        let {headline} = props.newsItems;
+        let {newsItems} = props;
 
         if (headline.indexOf('—') !== -1) {
             headline = headline.split('—')[0]
         }
 
-        console.log('props:', this.props);
-        console.log('newsItems:', this.props.newsItems);
+        console.log('newsItems:', newsItems);
         return (
             <div className="big-story col-xs-12">
                 <div className="col-sm-5">
-                    <h1>{headline}</h1>
-
-                    <p>{this.props.newsItems.metaDescription}</p>
-                    <TimeAndLink time={this.props.newsItems.timePosted} author={this.props.newsItems.author.username}/>
+                    <h1><HeadlineLink headline={headline} link={newsItems.link} /></h1>
+                    <p>{newsItems.metaDescription}</p>
+                    <TimeAndLink time={newsItems.timePosted} author={newsItems.author.username}/>
                 </div>
                 <div className="col-sm-7">
-                    <img src={this.props.newsItems.image} alt=""/>
+                    <img src={newsItems.image} alt=""/>
                 </div>
             </div>
         );
-    }
-});
+};
 
-var Main = React.createClass({
+var DatedListWithPics = (props) => {
+
+    let {items} = props;
+
+    let list = items.map((e, i) => {
+       return (
+           <div>
+               <div className="col-md-6" key={i}>
+                   <img src={e.image} alt=""/>
+               </div>
+               <div className="col-md-6">
+                    <h4>{e.headline}</h4>
+
+                   <TimeAndLink time={e.timePosted} author={e.author.username}/>
+               </div>
+           </div>
+       )
+    });
+
+    return (
+        <div className="col-md-6 col-lg-12">
+            {list}
+        </div>
+    );
+};
+
+var DatedListNoPics = (props) => {
+    console.log('items from DatesListNoPics:', props.items);
+
+    let {items} = props;
+
+    let list = items.map((e, i) => {
+        return (
+            <li className="col-sm-6" key={i}>
+                <h5>{e.headline}</h5>
+                <TimeAndLink time={e.timePosted} author={e.author.username} />
+            </li>
+        )
+    });
+
+    return (
+        <ul className="dated-list">
+            {list}
+        </ul>
+    );
+};
+
+let Main = React.createClass({
 
     getInitialState () {
         return {
@@ -181,7 +154,18 @@ var Main = React.createClass({
     },
 
     render () {
-        var loading = this.state.newsItems.length === 0;
+        const loading = this.state.newsItems.length === 0;
+        let listNoPics = [];
+        let listWithPics = [];
+
+        for(let i = 6; i <= 11; i++) {
+            listNoPics.push(this.state.newsItems[i]);
+        }
+
+        for(let i = 12; i <= 20; i++) {
+            listWithPics.push(this.state.newsItems[i]);
+        }
+
         return (
             <div className="container">
                 <Header />
@@ -214,10 +198,16 @@ var Main = React.createClass({
                                 : <SmallStory newsItems={this.state.newsItems[5]}/>
                             }
                         </div>
-                        <DatedListNoPics/>
+                        {loading
+                            ? <Loading />
+                            : <DatedListNoPics items={listNoPics}/>
+                        }
                     </div>
                     <div className="right-sided-lg-bottom-otherwise col-lg-4 col-md-12 col-sm-12 col-xs-12">
-                        <DatedListWithPics/>
+                        {loading
+                            ? <Loading />
+                            : <DatedListWithPics items={listWithPics}/>
+                        }
                     </div>
                 </div>
             </div>
